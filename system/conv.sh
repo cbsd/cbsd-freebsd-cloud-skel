e93611e0009cfb65364b2b89ae29ec4ed2ae0cf4
Charlie Root <root@freebsd1.my.domain>
2020-12-16 23:00:00 +0300

    wip

--- system/conv.sh
+++ system/conv.sh
@@ -1,6 +1,6 @@
 #/bi/nsh
 ver="12.2"
-fs="zfs"
+fs="ufs"
 rm -f /tmp/cloud-12.2_amd64.img
 cbsd jail2iso media=bhyve dstdir=/tmp efi=1 vm_guestfs=${fs} freesize=1800m jname=cloud applytpl=0
 echo "zfs destroy zroot/ROOT/default/cbsd-cloud-FreeBSD-${fs}-12.2-RELEASE-amd64.raw"
