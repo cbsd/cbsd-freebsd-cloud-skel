0f3b33ef788f46884dbb55fefc9b8b81aaeec48a
Charlie Root <root@goldbuild1.my.domain>
2021-07-22 21:00:50 +0200

    sync

--- system/conv.sh
+++ system/conv.sh
@@ -1,8 +1,9 @@
-#/bi/nsh
-ver="12.2"
-fs="ufs"
-rm -f /tmp/cloud-12.2_amd64.img
+#/bin/sh
+ver="13.0"
+#fs="ufs"
+fs="zfs"
+rm -f /tmp/cloud-13.0_amd64.img
 cbsd jail2iso media=bhyve dstdir=/tmp efi=1 vm_guestfs=${fs} freesize=1800m jname=cloud applytpl=0
-echo "zfs destroy zroot/ROOT/default/cbsd-cloud-FreeBSD-${fs}-12.2-RELEASE-amd64.raw"
-echo "rm -f /usr/jails/src/iso/cbsd-cloud-FreeBSD-${fs}-12.2-RELEASE-amd64.raw"
-echo "cp /tmp/cloud-12.2_amd64.img /usr/jails/src/iso/cbsd-cloud-FreeBSD-${fs}-12.2-RELEASE-amd64.raw"
+echo "zfs destroy zroot/ROOT/default/cbsd-cloud-FreeBSD-${fs}-13.0-RELEASE-amd64.raw"
+echo "rm -f /usr/jails/src/iso/cbsd-cloud-FreeBSD-${fs}-13.0-RELEASE-amd64.raw"
+echo "cp /tmp/cloud-13.0_amd64.img /usr/jails/src/iso/cbsd-cloud-FreeBSD-${fs}-13.0-RELEASE-amd64.raw"
